fe741635bd5d1cf0057f5d4372ddcd3c73db1161
Dan Purdy <dan@loftdigital.com>
2015-12-21 23:04:12 +0000

    :white_check_mark: Add specs

---
+++ spec/linter-sass-lint-scss-spec.js
@@ -0,0 +1,76 @@
+'use babel';
+
+describe('The scss_lint provider for Linter - sass', () => {
+  const lint = require('../lib/main').provideLinter().lint;
+  const configFile = __dirname + '/fixtures/config/.sass-lint.yml';
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+    waitsForPromise(() => {
+      atom.packages.activatePackage('linter-sass-lint');
+      return atom.packages.activatePackage('language-sass').then(() =>
+        atom.workspace.open(__dirname + '/fixtures/clean.scss')
+      );
+    });
+  });
+
+  describe('checks failure.sass and', () => {
+    let editor = null;
+    beforeEach(() => {
+      waitsForPromise(() => {
+        atom.config.set('linter-sass-lint.configPath', configFile);
+        return atom.workspace.open(__dirname + '/fixtures/files/failure.sass').then(openEditor => {
+          editor = openEditor;
+        });
+      });
+    });
+
+    it('finds at least one message', () => {
+      const messages = lint(editor);
+      expect(messages.length).toBeGreaterThan(0);
+    });
+
+    it('verifies the first message', () => {
+      const messages = lint(editor);
+      expect(messages[0].type).toBeDefined();
+      expect(messages[0].type).toEqual('Error');
+      expect(messages[0].html).toBeDefined();
+      expect(messages[0].html).toEqual('<span class=\"badge badge-flexible\">no-ids</span> ID selectors not allowed');
+      expect(messages[0].filePath).toBeDefined();
+      expect(messages[0].filePath).toMatch(/.+failure\.sass$/);
+      expect(messages[0].range).toBeDefined();
+      expect(messages[0].range.length).toEqual(2);
+      expect(messages[0].range).toEqual([[0, 0], [0, 1]]);
+    });
+
+    it('verifies the second message', () => {
+      const messages = lint(editor);
+      expect(messages[1].type).toBeDefined();
+      expect(messages[1].type).toEqual('Warning');
+      expect(messages[1].html).toBeDefined();
+      expect(messages[1].html).toEqual('<span class=\"badge badge-flexible\">no-color-literals</span> Color literals such as \'red\' should only be used in variable declarations');
+      expect(messages[1].filePath).toBeDefined();
+      expect(messages[1].filePath).toMatch(/.+failure\.sass$/);
+      expect(messages[1].range).toBeDefined();
+      expect(messages[1].range.length).toEqual(2);
+      expect(messages[1].range).toEqual([[1, 9], [1, 10]]);
+    });
+  });
+
+  describe('checks pass.sass and', () => {
+    let editor = null;
+    beforeEach(() => {
+      waitsForPromise(() => {
+        atom.config.set('linter-sass-lint.configPath', configFile);
+        return atom.workspace.open(__dirname + '/fixtures/files/pass.sass').then(openEditor => {
+          editor = openEditor;
+        });
+      });
+    });
+
+    it('finds nothing wrong with the valid file', () => {
+      const messages = lint(editor);
+      expect(messages.length).toEqual(0);
+    });
+  });
+});
