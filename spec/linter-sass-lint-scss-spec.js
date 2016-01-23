2ddb89a1b0a517c0966fc852972da6c86883cb28
Dan Purdy <dan@loftdigital.com>
2016-01-23 15:14:52 +0000

    :white_check_mark: Add extra specs closes #16

--- spec/linter-sass-lint-scss-spec.js
+++ spec/linter-sass-lint-scss-spec.js
@@ -8,9 +8,7 @@ describe('The scss_lint provider for Linter - scss', () => {
     atom.workspace.destroyActivePaneItem();
     waitsForPromise(() => {
       atom.packages.activatePackage('linter-sass-lint');
-      return atom.packages.activatePackage('language-sass').then(() =>
-        atom.workspace.open(__dirname + '/fixtures/clean.scss')
-      );
+      return atom.packages.activatePackage('language-sass');
     });
   });
 
@@ -73,6 +71,41 @@ describe('The scss_lint provider for Linter - scss', () => {
     });
 
     it('finds nothing wrong with the valid file', () => {
+      const messages = lint(editor);
+      expect(messages.length).toEqual(0);
+    });
+  });
+
+  describe('opens ignored.scss and', () => {
+    let editor = null;
+    beforeEach(() => {
+      waitsForPromise(() => {
+        atom.config.set('linter-sass-lint.configFile', configFile);
+        return atom.workspace.open(__dirname + '/fixtures/files/ignored.scss').then(openEditor => {
+          editor = openEditor;
+        });
+      });
+    });
+
+    it('ignores the file and reports no warnings', () => {
+      const messages = lint(editor);
+      expect(messages.length).toEqual(0);
+    });
+  });
+
+  describe('opens failure.scss and sets pacakage to not lint if no config file present', () => {
+    let editor = null;
+    beforeEach(() => {
+      waitsForPromise(() => {
+        atom.config.set('linter-sass-lint.noConfigDisable', true);
+        atom.config.set('linter-sass-lint.configFile', '');
+        return atom.workspace.open(__dirname + '/fixtures/files/failure.scss').then(openEditor => {
+          editor = openEditor;
+        });
+      });
+    });
+
+    it('doesn\'t lint the file as there\s no config file present', () => {
       const messages = lint(editor);
       expect(messages.length).toEqual(0);
     });
